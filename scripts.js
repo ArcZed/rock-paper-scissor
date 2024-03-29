ca12d936ce78f53f775d7e8fc2e08b86b4c8a134
Khoa Tran <92013921+ArcZed@users.noreply.github.com>
2024-03-30 02:55:11 +0700

    Add better UI

--- scripts.js
+++ scripts.js
@@ -1,15 +1,12 @@
 let playerScore = 0;
 let computerScore = 0;
 let isGameOver = false;
+let isEndRound = false
 
 const score = document.querySelector('.score');
 const compScore = document.querySelector('.computerScore');
 
 const resultScreen = document.querySelector('.resultScreen');
-
-const compRock = document.querySelector('.compRock');
-const compPaper = document.querySelector('.compPaper');
-const compScissors = document.querySelector('.compScissors');
 
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
@@ -18,23 +15,25 @@ const icon = document.querySelector('.icon');
 
 // Get player choice
 rock.addEventListener('click', (e)=>{
-    console.log(e.target.alt);
-    resetColor();
+    console.log(e);
+    clearScreen();
     playRound(e.target.alt, getComputerChoice());
     checkScore();
+    isEndRound = true;
 });
 paper.addEventListener('click', (e)=>{
-    console.log(e.target.alt);
-    resetColor();
+    console.log(e);
+    clearScreen();
     playRound(e.target.alt, getComputerChoice());
     checkScore();
+    isEndRound = true;
 });
 scissors.addEventListener('click',(e)=>{
-    console.log(e.target.alt);
-    resetColor();
+    console.log(e);
+    clearScreen();
     playRound(e.target.alt, getComputerChoice())
     checkScore();
-    
+    isEndRound = true;
 });
 
 // Generate computer's choice
@@ -43,15 +42,12 @@ function getComputerChoice(){
     if(!isGameOver){
     switch (choice){
         case 1: 
-            compRock.style.backgroundColor = 'bisque';
             return 'Rock';
         break;
         case 2: 
-            compPaper.style.backgroundColor = 'bisque';
             return 'Paper';
         break;
         case 3: 
-            compScissors.style.backgroundColor = 'bisque';
             return 'Scissors';
         break;
         default: 
@@ -64,29 +60,33 @@ function getComputerChoice(){
 
 //Play one round of RPS
 function playRound(playerOption, computerChoice){
-
-
+    
     let playerChoice = playerOption.toLowerCase();
     if(!isGameOver){
-    //win
-    if((playerChoice == 'rock' && computerChoice == 'Scissors') || 
-       (playerChoice == 'paper' && computerChoice == 'Rock') || 
-       (playerChoice == 'scissors' && computerChoice == 'Paper')){
-        playerScore++;
-        resultScreen.textContent = `You Win! ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)} beats ${computerChoice}`
+        //win
+        if((playerChoice == 'rock' && computerChoice == 'Scissors') || 
+        (playerChoice == 'paper' && computerChoice == 'Rock') || 
+        (playerChoice == 'scissors' && computerChoice == 'Paper')){
+            playerScore++;
+            resultScreen.textContent = `You Win! ${playerChoice} beats ${computerChoice}`
+        }
+        //tie
+        else if (playerChoice == computerChoice.toLowerCase()){
+            resultScreen.textContent = `You Tied! ${playerChoice} ties ${computerChoice}`
+        }
+        //lose
+        else {
+            computerScore++;
+            resultScreen.textContent = `You Lose! ${computerChoice} beats ${playerChoice}`
+        }
+        
     }
-    //tie
-    else if (playerChoice == computerChoice.toLowerCase()){
-        resultScreen.textContent = `You Tied! ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)} ties ${computerChoice}`
-    }
-    //lose
-    else {
-        computerScore++
-        resultScreen.textContent = `You Lose! ${computerChoice} beats ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)}`}
-}
     else{
         gameOver();
     }
+
+    createResult(playerChoice, computerChoice);
+   
 }
 // show the score and update the UI
 function checkScore(){
@@ -99,13 +99,10 @@ function checkScore(){
     }
 }
 
-function resetColor(){
-    compRock.style.backgroundColor = '';
-    compPaper.style.backgroundColor = '';
-    compScissors.style.backgroundColor = '';
-}
-
+// Create game over screen
 function gameOver(){
+
+    resultScreen.style.flexDirection = 'column';
     if(playerScore == 5){
         resultScreen.textContent = 'CONGRATULATION!!! YOU WON :]'
     }
@@ -113,6 +110,7 @@ function gameOver(){
         resultScreen.textContent = 'YOU LOST :[ TRY AGAIN?'
     }
 
+    //create restart button
     const playAgain = document.createElement('div');
     playAgain.classList.add('playAgain');
     playAgain.textContent = 'Play Again';
@@ -123,15 +121,70 @@ function gameOver(){
     });
 }
 
-
-
+// Reset game
 function resetGame(){
-    resetColor();
     score.textContent = '0';
     compScore.textContent = '0';
-    resultScreen.textContent = '';
+    resultScreen.innerHTML = '';
     playerScore = 0;
     computerScore = 0;
     isGameOver = false;
     console.log('Restart');
+}
+
+function createResult(playerChoice, computerChoice){
+
+    let resultContainer = document.createElement('div');
+    resultContainer.classList.add('resultContainer');
+    resultScreen.appendChild(resultContainer)
+
+    let playerResult = document.createElement('div');
+    playerResult.classList.add('icon');
+    resultContainer.appendChild(playerResult);
+
+    let playerImage = document.createElement('img');
+    playerImage.classList.add('img');
+    playerResult.appendChild(playerImage);
+    console.log(playerChoice)
+
+    switch (playerChoice) {
+        case 'rock':
+            playerImage.src = 'images/rocks.png';
+            break;
+        case 'paper':
+            playerImage.src = 'images/paper.png';
+            break;
+        case 'scissors':
+            playerImage.src = 'images/scissors.png';
+            break;
+        default:
+            break;
+    }
+
+    let computerResult = document.createElement('div');
+    computerResult.classList.add('icon');
+    resultContainer.appendChild(computerResult);
+
+    let computerImage = document.createElement('img');
+    computerImage.classList.add('img');
+    computerResult.appendChild(computerImage);
+    console.log(computerChoice);
+
+    switch (computerChoice) {
+        case 'Rock':
+            computerImage.src = 'images/rocks.png';
+            break;
+        case 'Paper':
+            computerImage.src = 'images/paper.png';
+            break;
+        case 'Scissors':
+            computerImage.src = 'images/scissors.png';
+            break;
+        default:
+            break;
+    }
+}
+
+function clearScreen(){
+    resultScreen.innerHTML = ''
 }
