72c31ea7e22d486521341b52b2c4ac3e08c5c4ca
Khoa Tran <92013921+ArcZed@users.noreply.github.com>
2024-03-29 21:43:47 +0700

    Add the logic and reset

--- scripts.js
+++ scripts.js
@@ -2,54 +2,76 @@ let playerScore = 0;
 let computerScore = 0;
 let isGameOver = false;
 
+const score = document.querySelector('.score');
+const compScore = document.querySelector('.computerScore');
+
 const resultScreen = document.querySelector('.resultScreen');
+
+const compRock = document.querySelector('.compRock');
+const compPaper = document.querySelector('.compPaper');
+const compScissors = document.querySelector('.compScissors');
+
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
 const icon = document.querySelector('.icon');
 
 // Get player choice
-
 rock.addEventListener('click', (e)=>{
     console.log(e.target.alt);
-    playRound(e.target.alt, getComputerChoice())
+    resetColor();
+    playRound(e.target.alt, getComputerChoice());
     checkScore();
 });
 paper.addEventListener('click', (e)=>{
     console.log(e.target.alt);
-    playRound(e.target.alt, getComputerChoice())
+    resetColor();
+    playRound(e.target.alt, getComputerChoice());
     checkScore();
 });
 scissors.addEventListener('click',(e)=>{
     console.log(e.target.alt);
+    resetColor();
     playRound(e.target.alt, getComputerChoice())
     checkScore();
+    
 });
 
 // Generate computer's choice
 function getComputerChoice(){
     let choice = Math.floor(Math.random()*3) + 1;
+    if(!isGameOver){
     switch (choice){
-        case 1: return 'Rock';
+        case 1: 
+            compRock.style.backgroundColor = 'bisque';
+            return 'Rock';
         break;
-        case 2: return 'Paper';
+        case 2: 
+            compPaper.style.backgroundColor = 'bisque';
+            return 'Paper';
         break;
-        case 3: return 'Scissor';
+        case 3: 
+            compScissors.style.backgroundColor = 'bisque';
+            return 'Scissors';
         break;
         default: 
         break;
     }
 }
+}
+
+
 
 //Play one round of RPS
 function playRound(playerOption, computerChoice){
 
+
     let playerChoice = playerOption.toLowerCase();
     if(!isGameOver){
     //win
-    if((playerChoice == 'rock' && computerChoice == 'Scissor') || 
+    if((playerChoice == 'rock' && computerChoice == 'Scissors') || 
        (playerChoice == 'paper' && computerChoice == 'Rock') || 
-       (playerChoice == 'scissor' && computerChoice == 'Paper')){
+       (playerChoice == 'scissors' && computerChoice == 'Paper')){
         playerScore++;
         resultScreen.textContent = `You Win! ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)} beats ${computerChoice}`
     }
@@ -66,13 +88,50 @@ function playRound(playerOption, computerChoice){
         gameOver();
     }
 }
-// Check the score
+// show the score and update the UI
 function checkScore(){
-    if(!gameOver && playerScore < 5 && computerScore < 5){
-        
+    score.textContent = playerScore;
+    compScore.textContent = computerScore;
+
+    if(playerScore == 5 || computerScore == 5){
+        isGameOver = true;
+        gameOver();
     }
 }
 
-function gameOver(){
+function resetColor(){
+    compRock.style.backgroundColor = '';
+    compPaper.style.backgroundColor = '';
+    compScissors.style.backgroundColor = '';
+}
 
+function gameOver(){
+    if(playerScore == 5){
+        resultScreen.textContent = 'CONGRATULATION!!! YOU WON :]'
+    }
+    else{
+        resultScreen.textContent = 'YOU LOST :[ TRY AGAIN?'
+    }
+
+    const playAgain = document.createElement('div');
+    playAgain.classList.add('playAgain');
+    playAgain.textContent = 'Play Again';
+    resultScreen.appendChild(playAgain);
+
+    playAgain.addEventListener('click', (e) =>{
+        resetGame()
+    });
+}
+
+
+
+function resetGame(){
+    resetColor();
+    score.textContent = '0';
+    compScore.textContent = '0';
+    resultScreen.textContent = '';
+    playerScore = 0;
+    computerScore = 0;
+    isGameOver = false;
+    console.log('Restart');
 }
